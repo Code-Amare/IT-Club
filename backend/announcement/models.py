71a54d6dce8e615d477e9cfd2a897e1124890331
Code-Amare <amisgana809@gmail.com>
2026-02-11 20:49:43 +0300

    feat: create announcement serializer

--- backend/announcement/models.py
+++ backend/announcement/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    announcement_date = models.DateField(null=True, blank=True)
+
+    targets = models.ManyToManyField(User, related_name="announcements", blank=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="created_announcements"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.announcement_date if self.announcement_date else 'No date'})"
---
+++ backend/announcement/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Announcement
+from users.serializers import UserInverseSerializer
+
+
+class AnnoucementSerializer(serializers.ModelSerializer):
+    created_by = UserInverseSerializer(read_only=True)
+    users = UserInverseSerializer(many=True, read_only=True, source="targets")
+
+    class Meta:
+        model = Announcement
+        fields = [
+            "id",
+            "title",
+            "announcement_date",
+            "targets",
+            "created_by",
+            "created_at",
+            "updated_at",
+            "is_important",
+        ]
